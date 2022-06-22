9a81ab28fa6a6114a7feec37144d2eaa7a9c09c4
Bader Alsabah <bhyss@hypertext.com.kw>
2022-06-22 14:10:26 +0400

    succesfully listenting and 1 route active

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-const express = require ('express')
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("You have successfully landed on ROOT");
+});
+
+server.listen(3000, () => console.log("Listening on port 3000"));
